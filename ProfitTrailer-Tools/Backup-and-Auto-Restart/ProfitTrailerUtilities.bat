f842278fe926770e65232004d1a8bc9c6a2a9394
clownfish44 <jreidsreef@gmail.com>
2018-01-28 01:14:16 -0500

    Update ProfitTrailerUtilities.bat

--- ProfitTrailer-Tools/Backup-and-Auto-Restart/ProfitTrailerUtilities.bat
+++ ProfitTrailer-Tools/Backup-and-Auto-Restart/ProfitTrailerUtilities.bat
@@ -1,22 +1,16 @@
-REM SETTING PATHS
+REM PRE SETUP
 set fromPath=%cd%\ProfitTrailer
 set to1Path=%cd%\ProfitTrailerBackup
+set reboottime=1
+set name=%RANDOM%
 
+REM START
 :start
 cls
 @echo off
-REM timeout interval in minutes for how often the backup will run.
-REM ==========================================================
-REM ===                                                    ===
-REM ===   CHANGE THIS TO DESIRED MINUTES BETWEEN BACKUPS   ===
-REM ===                     ||                             ===
-REM ===                     ||                             ===
-REM ===                    \  /                            ===
-REM ===                     \/                             ===
 set RUN_INTERVAL_IN_MINUTES=10
-set ramMB=512
-REM ===                                                    ===
-REM ==========================================================
+set ramMB=2024
+
 
 REM AUTO START ON PC CRASH/SHUTDOWN
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
@@ -32,7 +26,7 @@ del %SCRIPT%
 
 REM FIXING PT LAUNCH FILE
 del %cd%\ProfitTrailer\ProfitTrailer.cmd
-echo title PT215463463 ^& java ^-jar ProfitTrailer.jar ^-XX:+UseConcMarkSweepGC ^-Xmx%ramMB%m ^-Xms%ramMB%m>> %cd%\ProfitTrailer\ProfitTrailer.cmd
+echo title PT%name% ^& java ^-jar ProfitTrailer.jar ^-XX:+UseConcMarkSweepGC ^-Xmx%ramMB%m ^-Xms%ramMB%m>> %cd%\ProfitTrailer\ProfitTrailer.cmd
 
 REM MAKING EXCLUDED BACKUP
 del %cd%\ProfitTrailer\excludedfileslist.txt
@@ -67,6 +61,10 @@ mkdir %toPath%\%folderPath%
 
 xcopy "%fromPath%" "%toPath%" /e /h /k /exclude:%fromPath%\excludedfileslist.txt
 
+set /a reboottime="1+%reboottime%"
+if "%reboottime%"=="72" (goto reboot1)
+
+
 REM POST BACKUP MESSAGES AND PT CRASH CHECK
 :ars
 cls
@@ -78,16 +76,13 @@ echo This will take a min or so!
 echo We will return to the waiting for backup screen soon!
 
 @echo off
-tasklist /v /fo csv | findstr /i "PT215463463 " >nul
+tasklist /v /fo csv | findstr /i "PT%name% " >nul
 if %ERRORLEVEL% == 1 goto mycode
 goto eof
 :mycode
-echo ProfitTrailer either crashed or was not running!
-echo Starting now!
 cd %fromPath%
 start ProfitTrailer.cmd
-choice /d y /t 20 > nul
-goto ars
+goto eof
 
 :eof
 
@@ -101,11 +96,18 @@ echo ===========================================================================
 echo Last run on: %DATE% %TIME%
 echo ===========================================================================================
 
-REM =============================================================================
-REM ==      set RUN_INTERVAL_IN_MINUTES variable at the top of this file             
-REM ==      to desired minutes near the top of this batch file or replace 
-REM =============================================================================
 
 timeout /t %timeoutInSeconds%
 
 goto start
+
+:reboot1
+cls
+wmic Path win32_process Where "CommandLine Like '%%ProfitTrailer.jar%%'" Call Terminate
+timeout 2
+set reboottime=1
+xcopy "%toPath%" "%fromPath%" /e /h /k /y /exclude:%fromPath%\excludedfileslist.txt
+cd %fromPath%
+start ProfitTrailer.cmd
+goto eof
+
