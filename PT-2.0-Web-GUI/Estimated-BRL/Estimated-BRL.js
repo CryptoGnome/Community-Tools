8f7682aabdeb69a7703cae34806f1d7577e108e6
Deyvison Rocha <deyvison@gmail.com>
2018-04-23 15:11:44 -0300

    Estimated BRL

---
+++ PT-2.0-Web-GUI/Estimated-BRL/Estimated-BRL.js
@@ -0,0 +1,57 @@
+//Estimated USD difference for Profit Trailer 2.0
+//Created by ProfitTanker#0156 - If you need any help send me a DM.
+function estimate() {
+	//Pairs
+	if ($('#dtPairsLogs thead').length > 0) {
+		$('#dtPairsLogs tbody tr').each(function() {
+			$(this).find('b').remove();
+			var difference = ($(this).find('td.bought-cost:last .current-value').text().replace("R$", "").replace(",","") - $(this).find('td.bought-cost:last .bought-cost').text().replace("R$", "").replace(",","")).toFixed(2);
+			var difference2 = ($(this).find('td.current-value.blue-color:first .current-value').text().replace("R$", "").replace(",","") - $(this).find('td.current-value.blue-color:first .bought-cost').text().replace("R$", "").replace(",","")).toFixed(8);
+			if (difference2 > 0) {
+				$(this).find('td.bought-cost:last .bought-cost').append('<b style="color:#05b16f;border-top: 1px solid;"><br>R$' + difference + '</b>');
+				$(this).find('td.current-value.blue-color:first .bought-cost').append('<b style="color:#05b16f;border-top: 1px solid;"><br>' + difference2 + '</b>');
+			} else {
+				$(this).find('td.bought-cost:last .bought-cost').append('<b style="color:#d85353;border-top: 1px solid;"><br>R$' + difference + '</b>');
+				$(this).find('td.current-value.blue-color:first .bought-cost').append('<b style="color:#d85353;border-top: 1px solid;"><br>' + difference2 + '</b>');
+			}
+		});
+	}
+	//DCA
+	if ($('#dtDcaLogs thead').length > 0) {
+		$('#dtDcaLogs tbody tr').each(function() {
+			$(this).find('b').remove();
+			var difference = ($(this).find('td.current-value:last .current-value').text().replace("R$", "").replace(",","") - $(this).find('td.current-value:last .bought-cost').text().replace("R$", "").replace(",","")).toFixed(2);
+			var difference2 = ($(this).find('td.current-value.blue-color:first .current-value').text().replace("R$", "").replace(",","") - $(this).find('td.current-value.blue-color:first .bought-cost').text().replace("R$", "").replace(",","")).toFixed(8);
+			if (difference2 > 0) {
+				$(this).find('td.current-value:last').append('<b style="color:#05b16f;border-top: 1px solid;"><br>R$' + difference + '</b>');
+				$(this).find('td.current-value.blue-color:first').append('<b style="color:#05b16f;border-top: 1px solid;"><br>' + difference2 + '</b>');
+			} else {
+				$(this).find('td.current-value:last').append('<b style="color:#d85353;border-top: 1px solid;"><br>R$' + difference + '</b>');
+				$(this).find('td.current-value.blue-color:first').append('<b style="color:#d85353;border-top: 1px solid;"><br>' + difference2 + '</b>');
+			}
+		});
+	}
+	//Dust
+	if ($('#dtDustLogs thead').length > 0) {
+		$('#dtDustLogs tbody tr').each(function() {
+			$(this).find('b').remove();
+			var difference = ($(this).find('td.bought-cost .current-value').text().replace("R$", "").replace(",","") - $(this).find('td.bought-cost .bought-cost').text().replace("R$", "").replace(",","")).toFixed(2);
+			var difference2 = ($(this).find('td.blue-color.current-value:first .current-value').text().replace("R$", "").replace(",","") - $(this).find('td.blue-color.current-value:first .bought-cost').text().replace("R$", "").replace(",","")).toFixed(8);
+			if (difference2 > 0) {
+				$(this).find('td.bought-cost .bought-cost').append('<b style="color:#05b16f;border-top: 1px solid;"><br>R$' + difference + '</b>');
+				$(this).find('td.blue-color.current-value:first').append('<b style="color:#05b16f;border-top: 1px solid;"><br>' + difference2 + '</b>');
+			} else {
+				$(this).find('td.bought-cost .bought-cost').append('<b style="color:#d85353;border-top: 1px solid;"><br>R$' + difference + '</b>');
+				$(this).find('td.blue-color.current-value:first').append('<b style="color:#d85353;border-top: 1px solid;"><br>' + difference2 + '</b>');
+			}
+		});
+	}
+}
+$("body").on('DOMSubtreeModified', "#dvLastUpdatedOn", function() {
+	estimate();
+});
+$('#topNav a').on("click", function() {
+	setTimeout(function() {
+		estimate();
+	}, 100);
+});
